3efaa0daa94774057b3de967f0205263ac585997
chriskwlee <ckwl.default@gmail.com>
2014-12-17 00:58:43 -0500

    Implemented alpha-beta pruning

--- Minimax.py
+++ Minimax.py
@@ -1,26 +1,29 @@
-INFINITY = 100000
+class Minimax:
+	'Minimax implementation for chess moves'
 
-def minimax(state, depth):
-	if depth == 0 or state.isCheckmate() or state.isDraw():
-		return state.evaluate()
-	else: 
-		if state.toMove() == "white":
-			bestScore = -float(INFINITY)
-			bestMove = None
-			for move in state.allLegalMoves():
-				newState = state.makeMove(move)
-				score, move = minimax(newState, depth - 1)
-				if score > bestScore: 
-					bestScore = score
-					bestMove = move
-			return bestMove
-		else:
-			bestScore = float(INFINITY)
-			bestMove = None
-			for move in state.allLegalMoves():
-				newState = state.makeMove(move)
-				score, move = minimax(newState, depth - 1)
-				if score < bestScore
-					bestScore = score
-					bestMove = move
-			return bestMove
+	def minimax(state, alpha, beta):
+		if state.gameOver():
+			return state.evaluate()
+		else: 
+			if state.toMove() == "white":
+				bestMove = None
+				for move in state.allLegalMoves():
+					newState = state.makeMove(move)
+					score, move = minimax(newState, alpha, beta)
+					if score > alpha: 
+						alpha = score
+						bestMove = move
+						if alpha >= beta:
+							break
+				return bestMove
+			else:
+				bestMove = None
+				for move in state.allLegalMoves():
+					newState = state.makeMove(move)
+					score, move = minimax(newState, alpha, beta)
+					if score < beta
+						beta = score
+						bestMove = move
+						if alpha >= beta:
+							break
+				return bestMove
