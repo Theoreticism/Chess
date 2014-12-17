126eb2e73e1e6c132298b7e1ffa2df3a45fdf3ce
chriskwlee <ckwl.default@gmail.com>
2014-12-17 12:56:27 -0500

    Removed alpha-beta

--- Minimax.py
+++ Minimax.py
@@ -1,29 +1,27 @@
 class Minimax:
 	'Minimax implementation for chess moves'
 
-	def minimax(state, depth, alpha, beta):
+	def minimax(state, depth):
 		if depth == 0 or state.gameOver():
 			return [State.evaluate(), None]
 		else: 
 			if State.toMove() == "white":
+				bestScore = -float(100000)
 				bestMove = None
 				for move in State.allLegalMoves():
 					newState = State.makeMove(move)
-					score, move = minimax(newState, depth - 1, alpha, beta)
-					if score > alpha: 
-						alpha = score
+					score, move = minimax(newState, depth - 1)
+					if score > bestScore: 
+						bestScore = score
 						bestMove = move
-						if alpha >= beta:
-							break
-				return [alpha, bestMove]
+				return [bestScore, bestMove]
 			else:
+				bestScore = float(100000)
 				bestMove = None
 				for move in State.allLegalMoves():
 					newState = State.makeMove(move)
-					score, move = minimax(newState, alpha, beta)
-					if score < beta
-						beta = score
+					score, move = minimax(newState, depth - 1)
+					if score < bestScore
+						bestScore = score
 						bestMove = move
-						if alpha >= beta:
-							break
-				return [beta, bestMove]
+				return [bestScore, bestMove]
